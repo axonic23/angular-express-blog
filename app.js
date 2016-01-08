65a07ae6482da4d559e41d27a6316c9afcb7e3a2
Martin Welker <Welker@Axonic.net>
2016-01-08 14:40:26 +0100

    sdf
    sdfsdf

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
-var http = require('http');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  routes = require('./routes'),
+  api = require('./routes/api');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {
+    layout: false
+  });
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(process.env.PORT);
 
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+
+app.get('/api/posts', api.posts);
+
+app.get('/api/post/:id', api.post);
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Start server
+
+app.listen(process.env.PORT, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
